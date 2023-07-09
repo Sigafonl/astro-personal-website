32fd7665f55c5b4661e26f1f27373ef8908032fe
Noel <fsigafoos@gmail.com>
2023-07-09 14:46:15 -0700

    Polished the parallax a bit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				'3xl': '10px 15px 30px rgba(0, 0, 0, 0.40)',
+				'4xl': [
+					'0 35px 35px rgba(0, 0, 0, 0.25)',
+					'0 45px 65px rgba(0, 0, 0, 0.15)'
+				]
+			  }
+		},
 	},
 	plugins: [],
 }
